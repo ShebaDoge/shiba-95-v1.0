bb4c7de65f5a735526b38f94a5340a5ad9039f98
Sheba <64041679+ShebaDoge@users.noreply.github.com>
2023-01-11 16:25:54 -0500

    Update pswd.js

--- pswd.js
+++ pswd.js
@@ -12,7 +12,7 @@ alert("success!");
 }else if (pswd == "slope"){ //play slope
 alert("success!");
 var game = window.open("", "_self", "width=1500,height=1000");
-        game.document.write('<title>Google Classroom</title><iframe src="https://bigfoot.pages.dev/"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba/main.html">main</a><link href="../shiba/gmestyl.css" rel="stylesheet">');
+        game.document.write('<title>Google Classroom</title><iframe src="https://bigfoot.pages.dev/"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba/main.html">main</a><link href="https://raw.githubusercontent.com/ShebaDoge/shiba-95-v1.0/main/gmestyl.css" rel="stylesheet">');
         
 		
 }else if (pswd == "bb"){ //play basket bros
@@ -27,27 +27,27 @@ alert("success!");
 }else if (pswd == "gba"){ //play gba
 alert("success!");
 var game = window.open("", "_self", "width=1500,height=1000");
-        game.document.write('<iframe src="https://floofybull.github.io/GBA/"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba/main.html">main</a><link href="../shiba/gmestyl.css" rel="stylesheet">');
+        game.document.write('<iframe src="https://floofybull.github.io/GBA/"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba/main.html">main</a><link href="https://raw.githubusercontent.com/ShebaDoge/shiba-95-v1.0/main/gmestyl.css" rel="stylesheet">');
         game.document.write('</body></html>');
 	//<link rel="shortcut icon" href="Binaries/gb.ico">
 }else if (pswd == "mc"){ //https://unbl0ck3d-games.github.io/eaglercraft/stable-download/Offline_Download_Version.html
 alert("success!");
 var game = window.open("", "_self", "width=1500,height=1000");
-        game.document.write('<iframe src="https://unbl0ck3d-games.github.io/eaglercraft/stable-download/Offline_Download_Version.html"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba/main.html">main</a><link href="../shiba/gmestyl.css" rel="stylesheet">');
+        game.document.write('<iframe src="https://unbl0ck3d-games.github.io/eaglercraft/stable-download/Offline_Download_Version.html"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba/main.html">main</a><link href="https://raw.githubusercontent.com/ShebaDoge/shiba-95-v1.0/main/gmestyl.css" rel="stylesheet">');
         game.document.write('</body></html>');
 }else if (pswd == "cookie"){ //https://unbl0ck3d-games.github.io/Cookie-Clicker-Source-Code/
 alert("success!");
 	var game = window.open("", "_self", "width=1500,height=1000");
-        game.document.write('<iframe src="https://unbl0ck3d-games.github.io/Cookie-Clicker-Source-Code/"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba/main.html">main</a><link href="../shiba/gmestyl.css" rel="stylesheet">');
+        game.document.write('<iframe src="https://unbl0ck3d-games.github.io/Cookie-Clicker-Source-Code/"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba/main.html">main</a><link href="https://raw.githubusercontent.com/ShebaDoge/shiba-95-v1.0/main/gmestyl.css" rel="stylesheet">');
         game.document.write('</body></html>');
 } else if (pswd == "duck"){//duckhuntjs.com/
 var game = window.open("", "_self", "width=1500,height=1000");
-        game.document.write('<iframe src="duckhuntjs.com/"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba/main.html">main</a><link href="../shiba/gmestyl.css" rel="stylesheet">');
+        game.document.write('<iframe src="duckhuntjs.com/"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba/main.html">main</a><link href="https://raw.githubusercontent.com/ShebaDoge/shiba-95-v1.0/main/gmestyl.css" rel="stylesheet">');
         game.document.write('</body></html>');
   alert("success!");
 }else if (pswd == "paper"){//script.google.com/macros/s/AKfycbxO8jKpDOwFYpSGrGbReR4Mv2e1-6T4uHsWBphygjzyG8Qqpvij/exec
 var game = window.open("", "_self", "width=1500,height=1000");
-        game.document.write('<iframe src="script.google.com/macros/s/AKfycbxO8jKpDOwFYpSGrGbReR4Mv2e1-6T4uHsWBphygjzyG8Qqpvij/exec"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba/main.html">main</a><link href="../shiba/gmestyl.css" rel="stylesheet">');
+        game.document.write('<iframe src="script.google.com/macros/s/AKfycbxO8jKpDOwFYpSGrGbReR4Mv2e1-6T4uHsWBphygjzyG8Qqpvij/exec"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba/main.html">main</a><link href="https://raw.githubusercontent.com/ShebaDoge/shiba-95-v1.0/main/gmestyl.css" rel="stylesheet">');
         game.document.write('</body></html>');
   alert("success!");
 }else if (pswd === ""){ //no pin entered
