a72a8b14251b7dcf2c5b9f211f31f8b5c70c1c1a
Sheba <64041679+ShebaDoge@users.noreply.github.com>
2023-01-12 09:23:03 -0500

    Update pswd.js

--- pswd.js
+++ pswd.js
@@ -3,7 +3,7 @@ var pswd = prompt("Enter security PIN");
 if (pswd == "drift"){
 alert("success!");
         var game = window.open("", "_self", "width=1500,height=1000");
-        game.document.write('<title>Google Classroom</title><iframe src="https://grandcanyonshuttles.com/uploads/5/5/6/7/5567194/custom_themes/136734914403232081/drift_hunter.html"  width="100%" height="600px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));" style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba-95-v1.0/main.html">main</a><link href="../shiba/gmestyl.css" rel="stylesheet">');
+        game.document.write('<title>Google Classroom</title><iframe src="https://grandcanyonshuttles.com/uploads/5/5/6/7/5567194/custom_themes/136734914403232081/drift_hunter.html"  width="100%" height="600px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));" style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba-95-v1.0/main.html">main</a><link href="../shiba-95-v1.0/gmestyl.css" rel="stylesheet">');
        
 
 }else if (pswd == "cars"){ //play madalin stunt cars
@@ -27,7 +27,7 @@ alert("success!");
 }else if (pswd == "gba"){ //play gba
 alert("success!");
 var game = window.open("", "_self", "width=1500,height=1000");
-        game.document.write('<iframe src="https://floofybull.github.io/GBA/"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba-95-v1.0/main.html">main</a><link href="https://raw.githubusercontent.com/ShebaDoge/shiba-95-v1.0/main/gmestyl.css" rel="stylesheet">');
+        game.document.write('<iframe src="https://floofybull.github.io/GBA/"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba-95-v1.0/main.html">main</a><link href="../shiba-95-v1.0/gmestyl.css" rel="stylesheet">');
         game.document.write('</body></html>');
 	//<link rel="shortcut icon" href="Binaries/gb.ico">
 }else if (pswd == "mc"){ //https://unbl0ck3d-games.github.io/eaglercraft/stable-download/Offline_Download_Version.html
@@ -42,7 +42,7 @@ alert("success!");
         game.document.write('</body></html>');
 } else if (pswd == "duck"){//duckhuntjs.com/
 var game = window.open("", "_self", "width=1500,height=1000");
-        game.document.write('<iframe src="duckhuntjs.com/"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba-95-v1.0/main.html">main</a><link href="https://raw.githubusercontent.com/ShebaDoge/shiba-95-v1.0/main/gmestyl.css" rel="stylesheet">');
+        game.document.write('<iframe src="duckhuntjs.com/"  width="100%" height="700px" title="Iframe none" onload="javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));"style="height:900px;width:100%;border:none;overflow:hidden;border:none;"></iframe><br><br><br><a href="../shiba-95-v1.0/main.html">main</a><link href="../shiba-95-v1.0/gmestyl.css" rel="stylesheet">');
         game.document.write('</body></html>');
   alert("success!");
 }else if (pswd == "paper"){//script.google.com/macros/s/AKfycbxO8jKpDOwFYpSGrGbReR4Mv2e1-6T4uHsWBphygjzyG8Qqpvij/exec
